573dd62d680f981d2c31a4e7c3aaa50b114bcc78
bobbycyl <bobbycyl@126.com>
2024-07-27 23:47:38 +0800

    fix: do not update default radio values

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def run(g):
             break
         except Error as e:
             logger.get_logger("streamlit").exception(e)
+            # st.session_state.clear()
             break
         except Exception as e:
             st.error("uncaught exception: %s" % str(e))
@@ -167,13 +168,14 @@ got/100/95/90/85 {df["pp"].sum():.2f}/{df["b_pp_100if"].sum():.2f}/{df["b_pp_95i
         st.markdown("## Scatter Plot")
         begin_date, end_date = st.date_input("date range", [df["ts"].min() - pd.Timedelta(days=1), pd.Timestamp.today() + pd.Timedelta(days=1)])
         df1 = df[(df["ts"].dt.date > begin_date) & (df["ts"].dt.date < end_date)]
-        sr_slider = st.slider("SR", 0.0, 13.5, (0.5, 8.5))
+        sr_slider = st.slider("star rating", 0.0, 13.5, (0.5, 8.5))
         df2 = df1[(df1["b_star_rating"] > sr_slider[0]) & (df1["b_star_rating"] < sr_slider[1])]
         advanced_filter = st.text_input("advanced filter")
         if advanced_filter:
             df3 = df2.query(advanced_filter)
         else:
             df3 = df2
+            t1 = st.toast("advanced filter disabled")
         enable_size = st.checkbox("enable scatter plot size parameter")
         if "default_x_with_size_enabled" not in st.session_state:
             st.session_state.default_x_with_size_enabled = 25
@@ -185,16 +187,21 @@ got/100/95/90/85 {df["pp"].sum():.2f}/{df["b_pp_100if"].sum():.2f}/{df["b_pp_95i
         if enable_size:
             col1, col2 = st.columns(2)
             with col1:
-                x_radio = st.radio("x", df.columns, index=st.session_state.default_x_with_size_enabled)
+                x_radio_with_size_enabled = st.radio("x", df.columns, index=st.session_state.default_x_with_size_enabled)
             with col2:
                 size_radio = st.radio("size", df.columns, index=st.session_state.default_size)
             y_multiselect = st.multiselect("y", df.columns, default="score_nf")
             st.scatter_chart(
                 df3,
-                x=x_radio,
+                x=x_radio_with_size_enabled,
                 y=y_multiselect,
                 size=size_radio,
             )
+            st.session_state.default_x_with_size_enabled = pd.Index.get_loc(df.columns, x_radio_with_size_enabled)
+            st.session_state.default_size = pd.Index.get_loc(df.columns, size_radio)
+            t2 = st.toast("set default x to %s and default size to %s when size enabled" % (st.session_state.default_x_with_size_enabled, st.session_state.default_size))
+            time.sleep(1)
+            t2.empty()
         else:
             x_radio = st.radio("x", df.columns, index=st.session_state.default_x)
             y_multiselect = st.multiselect("y", df.columns, default="score_nf")
@@ -203,6 +210,12 @@ got/100/95/90/85 {df["pp"].sum():.2f}/{df["b_pp_100if"].sum():.2f}/{df["b_pp_95i
                 x=x_radio,
                 y=y_multiselect,
             )
+            st.session_state.default_x = pd.Index.get_loc(df.columns, x_radio)
+            t3 = st.toast("set default x to %s when size disabled" % st.session_state.default_x)
+            time.sleep(1)
+            t3.empty()
+        t1.empty()
+
     with st.container(border=True):
         st.markdown("## filtered data")
         st.dataframe(df3)
