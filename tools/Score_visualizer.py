adadf119246fc99eea3883811af91d7d13767c1e
bobbycyl <bobbycyl@126.com>
2024-08-02 10:52:47 +0800

    refactor: optimize code for better performance

--- tools/Score_visualizer.py
+++ tools/Score_visualizer.py
@@ -1,10 +1,10 @@
 import os
-from time import sleep
 
 import pandas as pd
 import streamlit as st
 
 from osuawa import Path
+from osuawa.utils import memorized_multiselect, memorized_selectbox
 
 st.set_page_config(page_title=_("Score visualizer") + " - osuawa")
 user = st.selectbox("user", [os.path.splitext(os.path.basename(x))[0] for x in os.listdir(os.path.join(str(Path.OUTPUT_DIRECTORY.value), Path.RECENT_SCORES.value))])
@@ -45,46 +45,29 @@ got/100/95/80h/80l {dfp["pp"].sum():.2f}/{dfp["b_pp_100if"].sum():.2f}/{dfp["b_p
             df3 = df2.query(advanced_filter)
         else:
             df3 = df2
-            t1 = st.toast(_("advanced filter disabled"))
         enable_size = st.checkbox(_("enable scatter plot size parameter"), key="cat_enable_size")
-        if "default_x_with_size_enabled" not in st.session_state:
-            st.session_state.default_x_with_size_enabled = 25
-        if "default_size" not in st.session_state:
-            st.session_state.default_size = 23
-        if "default_x" not in st.session_state:
-            st.session_state.default_x = 23
 
         if enable_size:
             col1, col2 = st.columns(2)
             with col1:
-                x_radio_with_size_enabled = st.radio("x", df.columns, index=st.session_state.default_x_with_size_enabled, key="cat_x_with_size_enabled")
+                memorized_selectbox("x", "cat_x2", df, 25)
             with col2:
-                size_radio = st.radio("size", df.columns, index=st.session_state.default_size, key="cat_size")
-            y_multiselect = st.multiselect("y", df.columns, default="score_nf", key="cat_y_with_size_enabled")
+                memorized_selectbox("s", "cat_s", df, 23)
+            memorized_multiselect("y", "cat_y2", df, ["score_nf"])
             st.scatter_chart(
                 df3,
-                x=x_radio_with_size_enabled,
-                y=y_multiselect,
-                size=size_radio,
+                x=st.session_state.cat_x2,
+                y=st.session_state.cat_y2,
+                size=st.session_state.cat_s,
             )
-            st.session_state.default_x_with_size_enabled = pd.Index.get_loc(df.columns, x_radio_with_size_enabled)
-            st.session_state.default_size = pd.Index.get_loc(df.columns, size_radio)
-            t2 = st.toast(_("set default x to %s and default size to %s when size enabled") % (x_radio_with_size_enabled, size_radio))
-            sleep(1)
-            t2.empty()
         else:
-            x_radio = st.radio("x", df.columns, index=st.session_state.default_x, key="cat_x")
-            y_multiselect = st.multiselect("y", df.columns, default="score_nf", key="cat_y")
+            memorized_selectbox("x", "cat_x", df, 23)
+            memorized_multiselect("y", "cat_y", df, ["score_nf"])
             st.scatter_chart(
                 df3,
-                x=x_radio,
-                y=y_multiselect,
+                x=st.session_state.cat_x,
+                y=st.session_state.cat_y,
             )
-            st.session_state.default_x = pd.Index.get_loc(df.columns, x_radio)
-            t3 = st.toast(_("set default x to %s when size disabled") % x_radio)
-            sleep(1)
-            t3.empty()
-        t1.empty()
 
     with st.container(border=True):
         st.markdown(_("## filtered data"))
