359a6e973fd559b746bb39d489e5b60a6846a776
bobbycyl <bobbycyl@126.com>
2024-08-02 10:10:36 +0800

    fix: update translations only needed to improve performance

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import builtins
-import gettext
-import locale
 import os.path
 
 import streamlit as st
@@ -17,11 +14,21 @@ if not os.path.exists(osuawa.Path.OUTPUT_DIRECTORY.value):
     os.mkdir(os.path.join(osuawa.Path.OUTPUT_DIRECTORY.value, osuawa.Path.RECENT_SCORES.value))
 
 if "lang" not in st.session_state:
-    t = gettext.translation("messages", localedir=osuawa.Path.LOCALE.value, languages=[locale.getlocale()[0]], fallback=True)
-else:
+    import locale
+
+    st.session_state._lang = locale.getlocale()[0]
+    st.session_state.lang = None
+
+if st.session_state.lang != st.session_state._lang:  # language changed
+    import gettext
+
+    st.session_state.lang = st.session_state._lang
     t = gettext.translation("messages", localedir=osuawa.Path.LOCALE.value, languages=[st.session_state.lang], fallback=True)
-st.session_state._ = t.gettext
-builtins.__dict__["_"] = st.session_state._
+
+    import builtins
+
+    st.session_state._ = t.gettext
+    builtins.__dict__["_"] = st.session_state._
 pg_homepage = st.Page("Home.py", title=_("Homepage"))
 pg_score_visualizer = st.Page("tools/Score_visualizer.py", title=_("Score visualizer"))
 pg_playlist_generator = st.Page("tools/Playlist_generator.py", title=_("Playlist generator"))
