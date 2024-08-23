7795d7ca18eeadd0ebb1b59e474ee5cc9e9f874b
bobbycyl <bobbycyl@126.com>
2024-08-23 17:46:40 +0800

    fix: update_locale.py now works correctly on Windows

--- update_locale.py
+++ update_locale.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 if __name__ == "__main__":
@@ -5,8 +6,12 @@ if __name__ == "__main__":
     langs = ["zh_CN"]
 
     for lang in langs:
-        path = "./share/locale/{}/LC_MESSAGES/".format(lang)
-        subprocess.run("pybabel extract -o {}/messages.po.new --input-dirs . --project=osuawa".format(path), shell=True, encoding="utf-8")
-        subprocess.run("rm messages.mo", shell=True, cwd=path, encoding="utf-8")
-        subprocess.run("msgmerge -U messages.po messages.po.new && rm messages.po.new && rm messages.po~", shell=True, cwd=path, encoding="utf-8")
-        subprocess.run("msgfmt messages.po", shell=True, cwd=path, encoding="utf-8")
+        path = "./share/locale/{}/LC_MESSAGES".format(lang)
+        subprocess.run("pybabel extract -o {}/messages.po.new --input-dirs . --project=osuawa".format(path), encoding="utf-8")
+        subprocess.run("msgmerge -U messages.po messages.po.new", cwd=path, encoding="utf-8")
+        try:
+            os.remove("{}/messages.po.new".format(path))
+            os.remove("{}/messages.po~".format(path))
+        except FileNotFoundError:
+            pass
+        subprocess.run("msgfmt messages.po", cwd=path, encoding="utf-8")
