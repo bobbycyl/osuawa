e127735f08f41a1cd4023de38f66fcc5d8363a3a
bobbycyl <bobbycyl@126.com>
2026-04-20 15:47:04 +0800

    fix: potential type coercion

--- osuawa/inject/row_style_with_dup.js
+++ osuawa/inject/row_style_with_dup.js
@@ -1,7 +1,7 @@
 (params) => {
     if (params.data._is_dup_bid) return {backgroundColor: "crimson"};
     if (params.data._is_dup_song) return {backgroundColor: "lemonchiffon"};
-    if (params.data.STATUS == 1) return {backgroundColor: "darkseagreen"};
-    if (params.data.STATUS == 2) return {backgroundColor: "powderblue"};
+    if (params.data.STATUS === 1) return {backgroundColor: "darkseagreen"};
+    if (params.data.STATUS === 2) return {backgroundColor: "powderblue"};
     return {};
 };
