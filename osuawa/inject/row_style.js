891482ed66ad78dacbee861c00eeeeeb8295b105
bobbycyl <bobbycyl@126.com>
2026-04-20 15:47:04 +0800

    fix: potential type coercion

--- osuawa/inject/row_style.js
+++ osuawa/inject/row_style.js
@@ -1,5 +1,5 @@
 (params) => {
-    if (params.data.STATUS == 1) return {backgroundColor: "darkseagreen"};
-    if (params.data.STATUS == 2) return {backgroundColor: "powderblue"};
+    if (params.data.STATUS === 1) return {backgroundColor: "darkseagreen"};
+    if (params.data.STATUS === 2) return {backgroundColor: "powderblue"};
     return {};
 };
