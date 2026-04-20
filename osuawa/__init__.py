35e1c2eae41eaffc6a87b52ff30168d8bf6ba2a3
bobbycyl <bobbycyl@126.com>
2026-04-20 16:48:07 +0800

    fix: auto set OSU_TOOLS_HOME environment variable at osuawa

--- osuawa/__init__.py
+++ osuawa/__init__.py
@@ -1,6 +1,11 @@
-import osupp
+import importlib
+import os
+
+osu_tools_home = os.path.abspath(os.path.join(str(os.path.dirname(__file__)), "..", "osu-tools"))
+if os.path.exists(osu_tools_home) and os.path.isdir(osu_tools_home):
+    os.environ["OSU_TOOLS_HOME"] = osu_tools_home
+
+importlib.import_module("osupp")
 
 from .osuawa import Awapi as Awapi, C as C, OsuPlaylist as OsuPlaylist, Osuawa as Osuawa
 from .utils import LANGUAGES as LANGUAGES
-
-assert osupp
