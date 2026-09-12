64509dfc8257e416d04126577fc647b9b9daeb12
bobbycyl <bobbycyl@126.com>
2026-09-12 23:09:55 +0800

    feat: dump mods script

---
+++ dump_mods.py
@@ -0,0 +1,12 @@
+import json
+
+from osuawa.utils import *
+
+def dump_mods(filename, obj):
+    with open(filename, "w", encoding="utf-8") as fo:
+        json.dump(obj, fo, indent=4, ensure_ascii=False)
+
+dump_mods("OsuModsEntries.json", osu_mod_entries)
+dump_mods("TaikoModsEntries.json", taiko_mod_entries)
+dump_mods("CatchModsEntries.json", catch_mod_entries)
+dump_mods("ManiaModsEntries.json", mania_mod_entries)
