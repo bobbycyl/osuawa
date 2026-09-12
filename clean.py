021fa3834c6712224a8980ca3744e4c5aa89fea1
bobbycyl <bobbycyl@126.com>
2026-09-12 13:04:45 +0800

    fix: bypass errors

--- clean.py
+++ clean.py
@@ -3,5 +3,5 @@ import shutil
 _ = lambda x: x
 from osuawa import C
 
-shutil.rmtree(C.LOGS.value)
-shutil.rmtree(C.UPLOADED_DIRECTORY.value)
+shutil.rmtree(C.LOGS.value, ignore_errors=True)
+shutil.rmtree(C.UPLOADED_DIRECTORY.value, ignore_errors=True)
